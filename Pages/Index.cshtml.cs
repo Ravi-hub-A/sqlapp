58a383e7e97eee7b9d96b08e07b696a73cafec94
KotaRavi <ravik.devops2@gmail.com>
2025-11-29 23:10:18 +0530

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlapp.Models;
+using sqlapp.Services;
+
+namespace sqlapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductService _productService;
+
+        public IndexModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public List<Product> Products { get; set; }
+
+        public async Task OnGet()
+        {
+            Products = await _productService.GetProductsAsync();
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration _configuration;
+
+        private static string db_source = "appserver600.database.windows.net";
+        private static string db_user = "sqlusr";
+        private static string db_password = "Azure@81";
+        private static string db_database = "appdb";
+
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = db_source;
+            builder.UserID = db_user;
+            builder.Password = db_password;
+            builder.InitialCatalog = db_database;
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            List<Product> productList = new List<Product>();
+
+            string query = "SELECT ProductID, ProductName, Quantity FROM Products";
+
+            using (SqlConnection connection = GetConnection())
+            {
+                await connection.OpenAsync();
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        Product product = new Product()
+                        {
+                            ProductID = reader.GetInt32(0),
+                            ProductName = reader.GetString(1),
+                            Quantity = reader.GetInt32(2)
+                        };
+
+                        productList.Add(product);
+                    }
+                }
+            }
+
+            return productList;
+        }
+    }
+}
+
+
